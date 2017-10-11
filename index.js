47539c28f18c4f1506162f60d8962d8506f89014
volodymyrtymets <volodymyrtymets@gmail.com>
2017-10-11 17:42:46 +0300

    add math

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+const _ = require('lodash');
 const logError = require('./src/log-error');
 const decoder = require('./src/wav-decoder');
 const decode = require('./src/wav-stream-decoder');
-const { getFrequencies } = require('./src/math');
+const { fft, getEnergy } = require('./src/fft');
 const getMic = require('./src/mic');
+var fs = require('fs');
 
-const FILE_NAME = 'output.wav';
+const FILE_NAME = './output.wav';
 
 const micSettings = {
   rate: 44100,
   channels: 2,
   bitwidth: 16,
-  debug: true,
+  debug: false,
   exitOnSilence: 6,
   device: 'plughw:1',
   fileType: 'wav',
@@ -28,24 +30,45 @@ const decodeFormat  =  {
 
 const mic = getMic(FILE_NAME, micSettings);
 
+let audioData = [];
 
 mic.micInputStream.on('data', function(data) {
-  console.log("Recieved Input Stream: " + data.length);
+  //console.log("Recieved Input Stream: " + data.length);
   decode(data).then(audio => {
-    console.log(audio.channelData[0].length);
-    const more = audio.channelData[0].find(value => value > 0);
-    console.log('more than 0 >', more);
+    //console.log(audio.channelData[0].length);
+
+    //const more = audio.channelData[0].find(value => value > 0);
+    //console.log('more than 0 >', more);
+
+    audioData = audioData.concat(_.values(audio.channelData[0]));
+
   }).catch(logError);
 });
 
 mic.micInputStream.on('stopComplete', () => {
-  // decoder(FILE_NAME)
-  //   .then(audioData => {
-  //     const frequencies = getFrequencies(audioData, 44100);
-  //     console.log(frequencies.slice(0, 10))
-  //   })
-  //   .catch(logError);
+  decoder(FILE_NAME)
+    .then(audioData => {
+      console.log(audioData.channelData[0].length)
+      const { spectrum }  = fft(_.values(audioData.channelData[0]));
+      const energy = getEnergy(spectrum , 10);
+      console.log('energy ->', energy)
+    })
+    .catch(logError);
 });
 
+
+const outputFileStream = fs.WriteStream(FILE_NAME);
+mic.micInputStream.pipe(outputFileStream);
+
 mic.micInstance.start();
-setTimeout(() => mic.micInstance.stop(), 2000);
+
+setTimeout(() => {
+  mic.micInstance.stop()
+  console.log(audioData.length)
+  const { spectrum }  = fft(audioData);
+  audioData = [];
+  const energy = getEnergy(spectrum , 10);
+  console.log('energy ->', energy)
+}, 1000);
+
+
---
+++ src/fft.js
@@ -0,0 +1,70 @@
+const _ = require('lodash');
+const math = require('mathjs');
+const fjs = require("frequencyjs");
+
+function nearestPow2( aSize ){
+  return Math.pow( 2, Math.round( Math.log( aSize ) / Math.log( 2 ) ) );
+}
+
+
+const getIndexOfMax = spectrum => {
+    let max = spectrum[0].amplitude;
+    let maxIndex = 0;
+
+    for (var i = 1; i < spectrum.length; i++) {
+        if (spectrum[i].amplitude > max) {
+            maxIndex = i;
+            max = spectrum[i].amplitude;
+        }
+    }
+    return maxIndex;
+};
+
+const getEnergy = (spectrum, l) => {
+  const indexOfMax = getIndexOfMax(spectrum);
+  // console.log('indexOfMax ->', indexOfMax);
+  // console.log('spectrum ->', spectrum[indexOfMax].amplitude);
+
+  // build arra to calculate energy +-l from max amplitude
+  const toCalculation = [];
+  let leftIndex = indexOfMax;
+  do {
+    toCalculation.push(spectrum[leftIndex]);
+    leftIndex--;
+  } while (spectrum[leftIndex] && (spectrum[leftIndex].frequency > spectrum[indexOfMax].frequency - l));
+  toCalculation.reverse();
+
+  let rightIndex = indexOfMax + 1;
+  do {
+    toCalculation.push(spectrum[rightIndex]);
+    rightIndex++;
+  } while (spectrum[rightIndex].frequency < spectrum[indexOfMax].frequency + l);
+
+
+  // calculate squere
+  const squeres = [];
+  for(let index = 0; index < toCalculation.length - 1; index ++) {
+    const a = toCalculation[index].amplitude;
+    const b = toCalculation[index + 1].frequency - toCalculation[index].frequency;
+    squeres.push(a * b);
+  }
+
+  return _.sum(squeres);
+};
+
+const fft = (audioData) => {
+  const lowRange = audioData;//audioData.channelData[0];
+  let waveLength = lowRange.length;
+  let index = nearestPow2(waveLength);
+
+  while (!(index <= lowRange.length)) {
+    waveLength = waveLength - 2;
+    index = nearestPow2(waveLength)
+  }
+
+  const wave = lowRange.slice(0, index);
+  const spectrum = fjs.Transform.toSpectrum(wave, { method: 'fft'} );
+  return { wave: _.values(wave), spectrum };
+};
+
+module.exports = { fft, getEnergy };
